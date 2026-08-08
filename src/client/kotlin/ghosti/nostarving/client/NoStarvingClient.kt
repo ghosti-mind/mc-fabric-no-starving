bc2298105191268b9a362a373f8bd4c997eeb25f
helsbekker <helsbekker@gmail.com>
2026-08-08 12:45:26 -0500

    Initial fabric template

---
+++ src/client/kotlin/ghosti/nostarving/client/NoStarvingClient.kt
@@ -0,0 +1,9 @@
+package ghosti.nostarving.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object NoStarvingClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
